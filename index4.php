4f772cf7dda285bf97ce6cd4b4da5fae7e975198
Nguyen7ung <68746959+Nguyen7ung@users.noreply.github.com>
2020-12-02 17:52:30 +0700

    Edit 4
    abc

--- index4.php
+++ index4.php
@@ -1,8 +1,15 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <p>Demo github 4</p>
+    </body>
+</html>
